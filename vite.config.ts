0d27e49d1d1e88f327d12ede8198f6cb3f7a85f4
Solar-gd <eng.altamirandus@gmail.com>
2026-01-03 08:22:30 -0300

    fix: Corrected base path in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: ´solarflowpro_',
+    base: ´solarflow pro_',
     server: {
     port: 3000,
       host: '0.0.0.0',
